d0fcf52b1c70b0f04d36b30559de81d942c9e533
Troy Venis <tvenis@Troys-MBP.home>
2016-09-16 23:15:19 -0500

    Yelp updates

--- web.py
+++ web.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template, request
-import weather
+import yelp_api
 import os
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-	address = request.values.get('address')
-	forecast = None
-	if address:
-			forecast = weather.get_weather(address)
-	return render_template('index.html', forecast=forecast)
+	location = request.values.get('location')
+	term = request.values.get('term')
+	lang = 'en'
+	if location:
+			restaurant = yelp_api.get_restaurant(term, location, lang)
+	else:
+			restaurant = None
+	return render_template('index.html', restaurant=restaurant)
 
 @app.route('/about')
 def about():
---
+++ yelp_api.py
@@ -0,0 +1,48 @@
+from yelp.client import Client
+from yelp.oauth1_authenticator import Oauth1Authenticator
+import os
+
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+auth = Oauth1Authenticator(
+    consumer_key=os.environ['CONSUMER_KEY'],
+    consumer_secret=os.environ['CONSUMER_SECRET'],
+    token=os.environ['TOKEN'],
+    token_secret=os.environ['TOKEN_SECRET'],
+)
+
+
+client = Client(auth)
+
+# term = 'food'
+# lang = 'en'
+# location = 'Southlake, TX'
+
+
+def get_restaurant(term, location, lang):
+	consumer_key = os.environ['CONSUMER_KEY']
+	consumer_secret=os.environ['CONSUMER_SECRET']
+	params = {
+    'term': term,
+    'lang': lang,
+    'limit': 3,
+    'sort': 2
+	}	
+
+	response = client.search(location, **params)
+
+	restaurants = []
+
+	for business in response.businesses:
+		print(business.name, business.rating, business.review_count, business.phone)
+		restaurants.append({"name":business.name,
+			 "rating":business.rating,
+			  "review_count":business.review_count})
+
+
+	return restaurants
+
+# restaurants = get_restaurant(term, location, lang)
+
+# print(restaurants)
